d98f631758c187e27993ce538e19cd543fac1e9a
kulgg <75735874+kulgg@users.noreply.github.com>
2022-11-02 21:19:26 +0100

    Fix layout props

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
+import React from "react";
 import "styles/globals.css";
 
-export default function RootLayout({ children }: { children: JSX.Element }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head />
