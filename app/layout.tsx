03ecf9358c6c54ebb8ad7a23920ea6fcec0f2351
kulgg <75735874+kulgg@users.noreply.github.com>
2022-11-02 18:03:02 +0100

    Make bg dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout({ children }: { children: JSX.Element }) {
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body className="bg-stone-900 text-gray-200">{children}</body>
     </html>
   );
 }
