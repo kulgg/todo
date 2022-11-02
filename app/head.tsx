c9244dc45264631b2070dcfc3c3c7f14f991b508
kulgg <75735874+kulgg@users.noreply.github.com>
2022-11-02 17:46:27 +0100

    Move to app

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>ToDo</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width" />
+      <meta
+        name="description"
+        content="An open source todo app built to test out the new Next.js 13 features."
+      />
+    </>
+  );
+}
